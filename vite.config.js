b95c35f2c6eacd485098cbcc9b000e40ea2fcecd
DanielAcevedoVega <antonydany236@gmail.com>
2024-10-09 11:22:16 -0500

    feat: :fire: Cambio de endpoit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://DanielAcevedoVega.github.io/proyectoConsultasMedicas/tree/git-page",
+  base: "https://DanielAcevedoVega.github.io/exampleDeploy",
 })
